5cebe3ae3c4c996069f3071129ea8e5ce1cdbfb3
ben <ich@abwesend.com>
2025-07-03 19:25:04 +0200

    fix formatting

--- bitstamp.lua
+++ bitstamp.lua
@@ -25,17 +25,17 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 
-WebBanking{
-  version = 1.1,
-  url = "https://www.bitstamp.net",
-  description = "Fetch balances from Bitstamp API and list them as securities",
-  services= { "Bitstamp Account" },
+WebBanking {
+    version = 1.1,
+    url = "https://www.bitstamp.net",
+    description = "Fetch balances from Bitstamp API and list them as securities",
+    services = { "Bitstamp Account" },
 }
 
 local apiKey
 local apiSecret
 local apiVersion = "v2"
-local currency = "EUR" -- fixme: Don't hardcode
+local currency = "EUR"     -- fixme: Don't hardcode
 local currencyName = "EUR" -- fixme: Don't hardcode
 local market = "Bitstamp"
 local accountName = "Balances"
@@ -44,145 +44,145 @@ local balances
 local customerId
 
 local currencyNames = {
-  BTC = "Bitcoin",
-  BCH = "Bitcoin Cash",
-  ETH = "Ether",
-  LTC = "Litecoin",
-  XRP = "Ripple",
-  EUR = "Euro",
-  USD = "US Dollar"
+    BTC = "Bitcoin",
+    BCH = "Bitcoin Cash",
+    ETH = "Ether",
+    LTC = "Litecoin",
+    XRP = "Ripple",
+    EUR = "Euro",
+    USD = "US Dollar"
 }
 
 function SupportsBank(protocol, bankCode)
-  return protocol == ProtocolWebBanking and bankCode == "Bitstamp Account"
+    return protocol == ProtocolWebBanking and bankCode == "Bitstamp Account"
 end
 
 function InitializeSession(protocol, bankCode, username, username2, password, username3)
-  apiKey = username2
-  apiSecret = password
-  customerId = username
+    apiKey = username2
+    apiSecret = password
+    customerId = username
 
-  balances = queryPrivate("balance")
+    balances = queryPrivate("balance")
 
-  prices = {
-    BTC = queryPublic("ticker/btceur"),
-    BCH = queryPublic("ticker/bcheur"),
-    ETH = queryPublic("ticker/etheur"),
-    LTC = queryPublic("ticker/ltceur"),
-    XRP = queryPublic("ticker/xrpeur"),
-    USD = invertPrices(queryPublic("ticker/eurusd")),
-    EUR = { vwap= 1 }
-  }
+    prices = {
+        BTC = queryPublic("ticker/btceur"),
+        BCH = queryPublic("ticker/bcheur"),
+        ETH = queryPublic("ticker/etheur"),
+        LTC = queryPublic("ticker/ltceur"),
+        XRP = queryPublic("ticker/xrpeur"),
+        USD = invertPrices(queryPublic("ticker/eurusd")),
+        EUR = { vwap = 1 }
+    }
 end
 
 function ListAccounts(knownAccounts)
-  local account = {
-    name = accountName,
-    accountNumber = accountNumber,
-    currency = currency,
-    portfolio = true,
-    type = "AccountTypePortfolio"
-  }
-  return {account}
+    local account = {
+        name = accountName,
+        accountNumber = accountNumber,
+        currency = currency,
+        portfolio = true,
+        type = "AccountTypePortfolio"
+    }
+    return { account }
 end
 
 function RefreshAccount(account, since)
-  local name
-  local currencyName
-  local s = {}
+    local name
+    local currencyName
+    local s = {}
 
-  for key, value in pairs(balances) do
-    if string.match(key, "_balance") then
-      currencyName, stringBalance = key:match("(.*)_(.*)")
-      currencyName = currencyName:upper()
-      name = currencyNames[currencyName] ~= nil and currencyNames[currencyName] or currencyName
+    for key, value in pairs(balances) do
+        if string.match(key, "_balance") then
+            currencyName, stringBalance = key:match("(.*)_(.*)")
+            currencyName = currencyName:upper()
+            name = currencyNames[currencyName] ~= nil and currencyNames[currencyName] or currencyName
 
-      if (prices[currencyName] ~= nil or key == currencyName) and tonumber(value) > 0 then
-        s[#s+1] = {
-          name = name,
-          market = market,
-          currency = nil,
-          quantity = value,
-          price = prices[currencyName] ~= nil and prices[currencyName]["vwap"] or 1
-        }
-      end
+            if (prices[currencyName] ~= nil or key == currencyName) and tonumber(value) > 0 then
+                s[#s + 1] = {
+                    name = name,
+                    market = market,
+                    currency = nil,
+                    quantity = value,
+                    price = prices[currencyName] ~= nil and prices[currencyName]["vwap"] or 1
+                }
+            end
+        end
     end
-  end
 
-  return {securities = s}
+    return { securities = s }
 end
 
 function EndSession()
 end
 
 function queryPrivate(method, request)
-  if request == nil then
-    request = {}
-  end
+    if request == nil then
+        request = {}
+    end
 
-  local path = string.format("/api/%s/%s/", apiVersion, method)
-  local nonce = string.format("%d", math.floor(MM.time() * 1000000))
-  local message = nonce .. customerId .. apiKey
-  local signature = string.upper(bin2hex(MM.hmac256(apiSecret, message)))
+    local path = string.format("/api/%s/%s/", apiVersion, method)
+    local nonce = string.format("%d", math.floor(MM.time() * 1000000))
+    local message = nonce .. customerId .. apiKey
+    local signature = string.upper(bin2hex(MM.hmac256(apiSecret, message)))
 
-  request["nonce"] = nonce
-  request["key"] = apiKey
-  request["signature"] = signature
+    request["nonce"] = nonce
+    request["key"] = apiKey
+    request["signature"] = signature
 
-  local postData = httpBuildQuery(request)
+    local postData = httpBuildQuery(request)
 
-  local connection = Connection()
-  local content = connection:request("POST", url .. path, postData, "application/x-www-form-urlencoded; charset=UTF-8")
+    local connection = Connection()
+    local content = connection:request("POST", url .. path, postData, "application/x-www-form-urlencoded; charset=UTF-8")
 
-  local json = JSON(content)
-  return json:dictionary()
+    local json = JSON(content)
+    return json:dictionary()
 end
 
 function queryPublic(method, request)
-  if request == nil then
-    request = {}
-  end
+    if request == nil then
+        request = {}
+    end
 
-  local path = string.format("/api/%s/%s/", apiVersion, method)
-  local queryString = httpBuildQuery(request)
-  local fullUrl = url .. path
-  if queryString ~= "" then
-    fullUrl = fullUrl .. "?" .. queryString
-  end
+    local path = string.format("/api/%s/%s/", apiVersion, method)
+    local queryString = httpBuildQuery(request)
+    local fullUrl = url .. path
+    if queryString ~= "" then
+        fullUrl = fullUrl .. "?" .. queryString
+    end
 
-  local connection = Connection()
-  local content = connection:request("GET", fullUrl)
-  local json = JSON(content)
+    local connection = Connection()
+    local content = connection:request("GET", fullUrl)
+    local json = JSON(content)
 
-  return json:dictionary()
+    return json:dictionary()
 end
 
 function bin2hex(s)
-  return (s:gsub(".", function (byte)
-    return string.format("%02x", string.byte(byte))
-  end))
+    return (s:gsub(".", function(byte)
+        return string.format("%02x", string.byte(byte))
+    end))
 end
 
 function httpBuildQuery(params)
-  local str = ''
-  for key, value in pairs(params) do
-    str = str .. key .. "=" .. tostring(value) .. "&"
-  end
-  if str == '' then
-    return ''
-  end
-  return str:sub(1, -2)
+    local str = ''
+    for key, value in pairs(params) do
+        str = str .. key .. "=" .. tostring(value) .. "&"
+    end
+    if str == '' then
+        return ''
+    end
+    return str:sub(1, -2)
 end
 
 function invertPrices(prices)
-  local newPrices = {}
-  for key, value in pairs(prices) do
-    local num = tonumber(value)
-    if num and num ~= 0 then
-      newPrices[key] = 1 / num
-    else
-      newPrices[key] = nil -- oder 0, wenn du lieber 0 möchtest
+    local newPrices = {}
+    for key, value in pairs(prices) do
+        local num = tonumber(value)
+        if num and num ~= 0 then
+            newPrices[key] = 1 / num
+        else
+            newPrices[key] = nil -- oder 0, wenn du lieber 0 möchtest
+        end
     end
-  end
-  return newPrices
+    return newPrices
 end
